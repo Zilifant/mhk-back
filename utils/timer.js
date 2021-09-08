e5b7b10f517aa6a8a2ddafc553063515da312e28
Scott Silsbe <scott.silsbe@gmail.com>
2021-09-08 16:53:01 -0400

    refactored timer to 10sec timer

--- utils/timer.js
+++ utils/timer.js
@@ -2,29 +2,47 @@
 
 const timers = {}
 
+// function run({lobbyId, duration, io}) {
+//   console.log(`${duration} minute timer started`);
+//   let timer = duration * 60,
+//       minutes,
+//       seconds;
+//   timers[lobbyId] = setInterval(() => {
+//       minutes = parseInt(timer / 60, 10);
+//       seconds = parseInt(timer % 60, 10);
+
+//       minutes = minutes < 10 ? '0' + minutes : minutes;
+//       seconds = seconds < 10 ? '0' + seconds : seconds;
+//       const string = minutes + ':' + seconds;
+
+//       if (!io) return console.log('Timer Error: no io');
+
+//       if (--timer < 0) {
+//         io.in(lobbyId).emit('tick', string);
+//         clearInterval(timers[lobbyId])
+//       }
+//       io.in(lobbyId).emit('tick', string);
+//       console.log(string);
+
+//   }, 1000);
+// };
+
 function run({lobbyId, duration, io}) {
-  console.log(`${duration} minute timer started`);
-  let timer = duration * 60,
-      minutes,
-      seconds;
+  console.log(`${duration} minute 10-sec timer started`);
+
+  let timer = duration * 6;
+
   timers[lobbyId] = setInterval(() => {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
+    if (!io) return console.log('Timer Error: no io');
 
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-      const string = minutes + ':' + seconds;
+    if (--timer < 0) {
+      io.in(lobbyId).emit('tenSec', timer);
+      clearInterval(timers[lobbyId])
+    }
+    io.in(lobbyId).emit('tenSec', timer);
+    console.log(timer);
 
-      if (!io) return console.log('Timer Error: no io');
-
-      if (--timer < 0) {
-        io.in(lobbyId).emit('tick', string);
-        clearInterval(timers[lobbyId])
-      }
-      io.in(lobbyId).emit('tick', string);
-      console.log(string);
-
-  }, 1000);
+  }, 10000);
 };
 
 function clear(lobbyId, io) {
