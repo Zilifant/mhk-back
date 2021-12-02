e87b11ffc1fcc39c601ddbbe26b3e5d46e30e9cb
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-02 18:02:35 -0500

    add comment

--- utils/uniqLobbyID.js
+++ utils/uniqLobbyID.js
@@ -1,10 +1,24 @@
+// Unique Lobby ID
+// Generates a randomized string of the pattern: adjective-noun-0000
 
 const ADJECTIVES = [
-  'aged', 'ancient', 'bitter', 'black', 'blue', 'broad', 'broken', 'calm', 'cold', 'crimson', 'damp', 'dark', 'delicate', 'dry', 'gentle', 'green', 'hidden', 'holy', 'frigid', 'vast', 'lucky', 'misty', 'nameless', 'old', 'great', 'patient', 'proud', 'purple', 'quiet', 'red', 'restless', 'rough', 'royal', 'shining', 'silent', 'snowy', 'lonely', 'foggy', 'still', 'summer', 'twilight', 'wandering', 'weathered', 'white', 'winter', 'yellow', 'scenic', 'shady', 'lost', 'rugged', 'majestic', 'splendid'
+  'aged', 'ancient', 'bitter', 'black', 'blue', 'broad', 'broken', 'calm',
+  'cold', 'crimson', 'damp', 'dark', 'delicate', 'dry', 'gentle', 'green',
+  'hidden', 'holy', 'frigid', 'vast', 'lucky', 'misty', 'nameless', 'old',
+  'great', 'patient', 'proud', 'purple', 'quiet', 'red', 'restless', 'rough',
+  'royal', 'shining', 'silent', 'snowy', 'lonely', 'foggy', 'still', 'summer',
+  'twilight', 'wandering', 'weathered', 'white', 'winter', 'yellow', 'scenic',
+  'shady', 'lost', 'rugged', 'majestic', 'splendid'
 ];
 
 const NOUNS = [
-  'crossing', 'reef', 'camp', 'village', 'alley', 'canyon', 'brook', 'passage', 'keep', 'tower', 'bridge', 'beach', 'bay', 'hollow', 'dream', 'planet', 'field', 'ruin', 'wall', 'garden', 'park', 'forest', 'depot', 'oasis', 'glade', 'hall', 'castle', 'fort', 'hill', 'lake', 'inlet', 'marsh', 'swamp', 'meadow', 'dock', 'mountain', 'peak', 'ocean', 'shoal', 'palace', 'barrens', 'pond', 'temple', 'way', 'island', 'river', 'farm', 'workshop', 'sea', 'villa', 'factory', 'waterfall', 'harbor', 'monolith', 'wood', 'outpost'
+  'crossing', 'reef', 'camp', 'village', 'alley', 'canyon', 'brook', 'passage',
+  'keep', 'tower', 'bridge', 'beach', 'bay', 'hollow', 'dream', 'planet',
+  'field', 'ruin', 'wall', 'garden', 'park', 'forest', 'depot', 'oasis',
+  'glade', 'hall', 'castle', 'fort', 'hill', 'lake', 'inlet', 'marsh', 'swamp',
+  'meadow', 'dock', 'mountain', 'peak', 'ocean', 'shoal', 'palace', 'barrens',
+  'pond', 'temple', 'way', 'island', 'river', 'farm', 'workshop', 'sea',
+  'villa', 'factory', 'waterfall', 'harbor', 'monolith', 'wood', 'outpost'
 ];
 
 const Haikunator = require('haikunator');
