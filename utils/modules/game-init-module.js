5accebbb5d9d8bcbe389ff8f56ff7b5062ec182c
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-02 19:12:25 -0500

    add comments

--- utils/modules/game-init-module.js
+++ utils/modules/game-init-module.js
@@ -1,5 +1,5 @@
 // Game Initiation Module
-// creates new game
+// Provides functions used to create a new game.
 
 const sample = require('lodash.sample');
 
@@ -9,12 +9,14 @@ const {
   nullify, shuffle, shuffleAndBatch, makeGhostCard, OPT_ROLES, HIDE_FROM, HUNTER, KILLER, GHOST, ACCOMPLICE, WITNESS, EVIDENCE_DECK, MEANS_DECK, GHOST_CARD_INFO,
 } = require('../utils');
 
+// This is added to lobby as a method.
 function makeGame() {
 
+  // TO DO: refactor/remove redundant properties.
   const game = {
-    lobbyId: this.id,
-    settings: this.gameSettings,
-    players: this.usersReady(),
+    lobbyId: this.id, // lobby.id
+    settings: this.gameSettings, // lobby.gameSettings
+    players: this.usersReady(), // lobby.usersReady()
     confirmedClues: [],
     rolesRef: [],
     spectators: [],
@@ -30,7 +32,9 @@ function makeGame() {
     keyEvidence: [],
     result: null,
     isResolvingAccusal: false,
+    timerIsRunning: false,
     currentStage: g.STAGES[0],
+
     advanceStage(stageId, io) {
 
       if (stageId) {
@@ -43,28 +47,32 @@ function makeGame() {
       if (this.settings.timer.on) g.handleTimer(this, io);
 
     },
+
+    // Redacts some properties based on given role.
     viewAs(role) {
       const g = nullify(this, HIDE_FROM[role]);
       g.viewingAs = role;
       return g;
     },
+
     blueCanAccuse() {
       return this.blueTeam.some(player => !!player.canAccuse);
-    },
-    timerIsRunning: false,
+    }
   };
 
   initRoles(game);
   createHands(game);
   createGhostCardDisplay(game);
 
-  this.game = game;
-  this.gameOn = true;
+  this.game = game; // lobby.game
+  this.gameOn = true; // lobby.gameOn
 };
+
+// Player Roles //
 
 function initRoles(game) {
   selectGhost(game);
-  assignNGRoles(game);
+  assignNonGhostRoles(game);
   createRolesRef(game);
   createTeamsRef(game);
 };
@@ -82,13 +90,18 @@ function randomGhost(game) {
   return game.ghost = sample(game.players);
 };
 
-function initNGRoles(game) {
+// Return an array of roles needed (including multiple Hunters) based on game
+// settings and player count.
+function initNonGhostRoles(game) {
   game.nonGhosts = getNonGhosts(game);
+
   const roles = [KILLER];
   if (game.settings.hasWitness) roles.push(WITNESS);
   if (game.settings.hasAccomplice) roles.push(ACCOMPLICE);
+
   const numHunters = game.nonGhosts.length - roles.length;
   for (let i = 0; i < numHunters ; i++) roles.push(HUNTER);
+
   return roles;
 };
 
@@ -96,10 +109,11 @@ function getNonGhosts(game) {
   return game.players.filter(player => player.id !== game.ghost.id);
 };
 
-function assignNGRoles(game) {
-  const shuffledRoles = shuffle(initNGRoles(game));
+function assignNonGhostRoles(game) {
+  const shuffledRoles = shuffle(initNonGhostRoles(game));
+
   game.nonGhosts.forEach((nG, index) => {
-    nG.canAccuse = true;
+    nG.canAccuse = true; // TO DO: remove semi-related side effect
     if (shuffledRoles[index] === KILLER) return game.killer = nG;
     if (shuffledRoles[index] === WITNESS) return game.witness = nG;
     if (shuffledRoles[index] === ACCOMPLICE) return game.accomplice = nG;
@@ -107,12 +121,20 @@ function assignNGRoles(game) {
   });
 };
 
+// Roles Reference Objects //
+// Used by other functions that need to know player roles and teams. Not part
+// of core game logic.
+// Ideally, future refactorings will make these unnecessary.
+
 function createRolesRef(game) {
+  // 1 Ghost and 1 Killer are always assigned;
   game.rolesRef = [
     {role: GHOST, user: game.ghost},
     {role: KILLER, user: game.killer},
   ];
+  // 1 or more Hunters are always assigned.
   game.hunters.forEach(h => game.rolesRef.push({role: HUNTER, user: h}));
+  // 0 or 1 of each optional role (Witness, Accomplice) may be assigned.
   OPT_ROLES.forEach(role => {
     if (!!game[role]) game.rolesRef.push({role: role, user: game[role]});
   });
@@ -121,8 +143,13 @@ function createRolesRef(game) {
 function createTeamsRef(game) {
   game.blueTeam = [game.ghost, game.witness, game.hunters].flat().filter(x => !!x);
   game.redTeam = [game.killer, game.accomplice].filter(x => !!x);
+  // Note: this filters out falsy values in case of unused optional roles
 };
 
+// Player Hands //
+
+// ShuffleAndBatch returns an array of 'batches'. Batches are given to each
+// player based on index (the idx of the batch and of the player);
 function createHands(game) {
   const meansCards = shuffleAndBatch(MEANS_DECK, 4),
         evidenceCards = shuffleAndBatch(EVIDENCE_DECK, 4);
@@ -132,8 +159,11 @@ function createHands(game) {
   });
 };
 
+// Ghost Cards //
+
 function createGhostCardDisplay(game) {
   const GHOST_CARDS = GHOST_CARD_INFO.map(item => makeGhostCard(item));
+
   const CAUSES_DECK = GHOST_CARDS.filter(card => card.type === 'cause');
   const LOCS_DECK   = GHOST_CARDS.filter(card => card.type === 'location');
   const CLUES_DECK  = GHOST_CARDS.filter(card => card.type === 'clue');
@@ -144,12 +174,13 @@ function createGhostCardDisplay(game) {
   const locationCard = sample(LOCS_DECK);
   locationCard.isDisplayed = true;
 
+  // Shuffle Clue cards and select the first 6 to start the Clues Deck.
   game.cluesDeck = shuffle(CLUES_DECK).filter((card, index) => index < 6);
-
+  // First 4 Clue cards are displayed; last 2 are hidden.
   game.cluesDeck.forEach((card, index) => {
     if (index < 4) card.isDisplayed = true;
   });
-
+  // Add the Cause and Location cards to the front of the Clues Deck.
   game.cluesDeck.unshift(causeCard, locationCard);
 };
 
