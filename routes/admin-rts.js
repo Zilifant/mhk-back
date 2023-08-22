bc24318c9ccb9311b8710b1ec881a7e0765d2325
Zilifant <scott.silsbe@gmail.com>
2023-08-22 10:58:34 -0400

    add test cookies route

--- routes/admin-rts.js
+++ routes/admin-rts.js
@@ -13,14 +13,25 @@ const getLobbies = async (req, res, next) => {
 // ***** Not used by MHK app. *****
 // Used by personal portfolio website to fetch content from JSON database.
 const getPortfolio = async (req, res, next) => {
-  sendNodeHttpsRequest({
-    hostname: 'api.jsonbin.io',
-    path: `/v3/b/${process.env.PORTFOLIO_BIN_ID}/latest`,
-    headers: { 'X-Master-Key': process.env.JSIO_KEY }
-  }, (data) => res.json({ data }));
+  sendNodeHttpsRequest(
+    {
+      hostname: 'api.jsonbin.io',
+      path: `/v3/b/${process.env.PORTFOLIO_BIN_ID}/latest`,
+      headers: { 'X-Master-Key': process.env.JSIO_KEY },
+    },
+    (data) => res.json({ data }),
+  );
+};
+
+const getCookies = async (req, res, next) => {
+  const cookies = req.get('Cookie');
+  console.log('cookies:', cookies);
+
+  res.json({ cookies: cookies });
 };
 
 router.get('/lobbies', getLobbies);
 router.get('/portfolio', getPortfolio);
+router.get('/cookies', getCookies);
 
 module.exports = router;
